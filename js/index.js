aa3e1243c6fc445812e2f3b2d52b8b9bc709d8c0
Tommy <tommy_cuang@yahoo.co.id>
2015-11-29 22:47:36 +0700

    revisi geometri.html

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 var button = document.getElementsByClassName("btn-bwh");
 var temp=button[0].style.backgroundColor;
 function atas(x){
-    button[x].parentNode.parentNode.style.transition="all 1s";
+    button[x].parentNode.parentNode.parentNode.style.transition="all 1s";
     document.getElementById("isi").style.transition="all 1s";
     document.getElementById("isi").style.backgroundColor="lightgrey";
-    button[x].parentNode.parentNode.style.backgroundColor="green";
-    button[x].parentNode.parentNode.style.opacity=1;
+    button[x].parentNode.parentNode.parentNode.style.backgroundColor="green";
+    button[x].parentNode.parentNode.parentNode.style.opacity=1;
 }
 function keluar(x){
     document.getElementById("isi").style.transition="all 1s";
     document.getElementById("isi").style.backgroundColor=temp;
-    button[x].parentNode.parentNode.style.backgroundColor=temp;
-    button[x].parentNode.parentNode.style.opacity=1;
+    button[x].parentNode.parentNode.parentNode.style.backgroundColor=temp;
+    button[x].parentNode.parentNode.parentNode.style.opacity=1;
 }
